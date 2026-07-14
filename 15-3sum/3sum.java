e6de79297c2c6e5cf18f5108bec5654618366191
HarshadaChaudhari09 <harshu1151@gmail.com>
2026-07-15 00:14:22 +0530

    Time: 33 ms (73.93%) | Memory: 59.1 MB (59.84%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -15,7 +15,7 @@ class Solution {
             int right = nums.length - 1;
 
             while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
+                int sum = nums[i] + nums[left] + nums[right]; 
 
                 if (sum == 0) {
                     result.add(Arrays.asList(nums[i], nums[left], nums[right]));
